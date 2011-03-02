fe7825ac454f0a0446e13622275ad50565dc153b
Thomas Pelletier <pelletier.thomas@gmail.com>
2011-03-02 11:33:54 +0100

    Switched to jQuery: we are humans, not masochists

--- templatetags/dynamictwain.py
+++ templatetags/dynamictwain.py
@@ -6,6 +6,10 @@ register = template.Library()
 
 @register.inclusion_tag('dynamictwain/widget.html')
 def scan_widget(size="600x800", multiple=True):
+    if multiple == "False":
+        multiple = False
+    elif multiple == "True":
+        multiple = True
     size = size.split('x')
     width = size[0]
     height = size[1]
@@ -15,6 +19,9 @@ def scan_widget(size="600x800", multiple=True):
         multiple = 'false'
     data = {
         'base_url': getattr(settings,'DYNAMIC_TWAIN_MEDIA_ROOT',"/site_media/dynamic_twain/"),
+        'default_resolution': getattr(settings,'DYNAMIC_TWAIN_DEFAULT_RESOLUTION', 150),
+        'server': getattr(settings, 'DYNAMIC_TWAIN_SERVER', 'localhost'),
+        'upload_path': getattr(settings, 'DYNAMIC_TWAIN_UPLOAD_PATH', '/twain/upload/'),
         'width': width,
         'height': height,
         'multiple': multiple,
